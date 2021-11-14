17828d2ba05a7ba13bed750faab454fb598e89d3
collinsJ24 <james.collins36@mail.dcu.ie>
2021-11-14 11:39:32 +0000

    VIN search bar added, CSS changes

--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Title.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function Title() {
   return (
     <div className="Title">
       <div className="heading">Similar Car</div>
+      <div class="search">
+      <input type="text" class="searchTerm" placeholder="Enter VIN To Retrieve Vehicle Details & Market Valuation For Your Car"/>
+      <button type="submit" class="searchButton">
+      <FontAwesomeIcon icon={ faSearch }/>
+     </button>
+   </div>
     </div>
   )
 }
